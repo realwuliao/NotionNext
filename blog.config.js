cd02b02909754b172d886a9877f978d918c5c9a5
realwuliao <25335782+realwuliao@users.noreply.github.com>
2023-10-14 01:23:45 +0800

    2

--- blog.config.js
+++ blog.config.js
@@ -188,7 +188,7 @@ const BLOG = {
   WIDGET_PET: process.env.NEXT_PUBLIC_WIDGET_PET || true, // 是否显示宠物挂件
   WIDGET_PET_LINK:
         process.env.NEXT_PUBLIC_WIDGET_PET_LINK ||
-        'https://xxx.tgftgf.workers.dev/100100/model.json', // 挂件模型地址 @see https://github.com/xiazeyu/live2d-widget-models
+        'https://yt.xysm.cf/100100/model.json', // 挂件模型地址 @see https://github.com/xiazeyu/live2d-widget-models
   WIDGET_PET_SWITCH_THEME: process.env.NEXT_PUBLIC_WIDGET_PET_SWITCH_THEME || true, // 点击宠物挂件切换博客主题
 
   // 音乐播放插件
