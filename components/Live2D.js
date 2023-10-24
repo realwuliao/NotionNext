b22a10179694a94652ff10c0bcb135667ecc5b59
realwuliao <25335782+realwuliao@users.noreply.github.com>
2023-10-25 01:27:10 +0800

    0

--- components/Live2D.js
+++ components/Live2D.js
@@ -1,53 +1,71 @@
-/* eslint-disable no-undef */
-import BLOG from '@/blog.config'
-import { useGlobal } from '@/lib/global'
-import { loadExternalResource } from '@/lib/utils'
-import { useEffect } from 'react'
-
+import React, { useEffect, useState } from 'react';
+import * as PIXI from 'pixi.js'
 export default function Live2D() {
-  const { theme, switchTheme } = useGlobal()
-  const showPet = JSON.parse(BLOG.WIDGET_PET)
+  const modelUrl = 'https://xxx.tgftgf.workers.dev/103302/model.json';
+  const [modelData, setModelData] = useState(null);
+  const follow = true;
 
   useEffect(() => {
-    if (showPet) {
-      Promise.all([
-        loadExternalResource('https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/live2d.min.js', 'js')
-      ]).then((e) => {
-        if (typeof window?.loadlive2d !== 'undefined') {
-          // https://github.com/xiazeyu/live2d-widget-models
-          try {
-            loadlive2d('live2d', BLOG.WIDGET_PET_LINK)
-          } catch (error) {
-            console.error('读取PET模型', error)
-          }
-        }
+    // 使用 Fetch API 从网络链接获取 Live2D 模型资源
+    fetch(modelUrl)
+      .then((response) => response.json()) // 或者使用适当的解析方法
+      .then((data) => {
+        // 数据加载成功后，设置模型数据
+        setModelData(data);
       })
+      .catch((error) => {
+        console.error('加载模型资源失败', error);
+      });
+  }, [modelUrl]);
+
+  useEffect(() => {
+    if (modelData) {
+      // 创建 Live2D 模型
+      const settings = new PIXI.live2d.Cubism4ModelSettings(modelData);
+      const live2dSprite = PIXI.live2d.Live2DModel.from(settings, {
+        eyeBlink: true,
+        lipSyncWithSound: true,
+        debugLog: false,
+        debugMouseLog: false,
+        randomMotion: false,
+        defaultMotionGroup: 'Motion',
+        autoInteract: follow,
+        expressionFadingDuration: 0,
+        motionFadingDuration: 0,
+        idleMotionFadingDuration: 0
+      });
+
+      // 创建 Pixi Application
+      const app = new PIXI.Application({
+        width: 1280,
+        height: 1600,
+        transparent: true,
+        preserveDrawingBuffer: true
+      });
+
+      // 将 Live2D 模型添加到 Pixi Application 的舞台
+      app.stage.addChild(live2dSprite);
+      live2dSprite.scale.set(0.5, 0.5);
+
+      // 点击事件处理
+      function handleModelClick() {
+        // 随机切换表情
+        live2dSprite.internalModel.motionManager.expressionManager.setRandomExpression();
+
+        // 随机切换动作
+        live2dSprite.internalModel.motionManager.startRandomMotion('Motion');
+      }
+
+      // 将点击事件处理函数绑定到 Live2D 模型
+      live2dSprite.on('click', handleModelClick);
+
+      // 将 Pixi Application 挂载到页面上的 DOM 元素
+      const container = document.getElementById('live2d-container');
+      container.style.width = '320px';
+      container.style.height = '400px';
+      container.appendChild(app.view);
     }
-  }, [theme])
+  }, [modelData]);
 
-  function handleClick() {
-    if (JSON.parse(BLOG.WIDGET_PET_SWITCH_THEME)) {
-      switchTheme()
-    }
-  }
-
-  if (!showPet) {
-    return <></>
-  }
-
-  return (
-    <canvas
-      id="live2d"
-      width="1280"
-      height="1600"
-      onClick={handleClick}
-      className="cursor-grab"
-      onMouseDown={(e) => e.target.classList.add('cursor-grabbing')}
-      onMouseUp={(e) => e.target.classList.remove('cursor-grabbing')}
-      style={{
-        width: '320px', // 设置Canvas的CSS宽度
-        height: '400px' // 设置Canvas的CSS高度
-      }}
-    />
-  );
+  return <div id='live2d-container' style={{ width: '320px', height: '400px' }}></div>;
 }
