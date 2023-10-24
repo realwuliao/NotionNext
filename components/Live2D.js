4ff37742a2206b669d46b7ca242f10e952085664
realwuliao <25335782+realwuliao@users.noreply.github.com>
2023-10-24 15:54:19 +0800

    1

--- components/Live2D.js
+++ components/Live2D.js
@@ -44,10 +44,18 @@ export default function Live2D() {
     return <></>
   }
 
-  return <canvas id="live2d" width="280" height="250" onClick={handleClick}
-        className="cursor-grab"
-        onMouseDown={(e) => e.target.classList.add('cursor-grabbing')}
-        onMouseUp={(e) => e.target.classList.remove('cursor-grabbing')}
-    />
-  )
+  return
+  <canvas
+    id="live2d"
+    width="900"
+    height="900"
+    onClick={handleClick}
+    className="cursor-grab"
+    onMouseDown={(e) => e.target.classList.add('cursor-grabbing')}
+    onMouseUp={(e) => e.target.classList.remove('cursor-grabbing')}
+    style={{
+      width: '300px', // 设置Canvas的CSS宽度
+      height: '300px' // 设置Canvas的CSS高度
+    }}
+  />
 }
