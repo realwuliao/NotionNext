66fece5d88ab2a229265474d9ce3e95b19774091
realwuliao <25335782+realwuliao@users.noreply.github.com>
2023-10-25 14:06:42 +0800

    0

--- components/Live2D.js
+++ components/Live2D.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { loadExternalResource } from '@/lib/utils'
+import { loadExternalResource } from '@/lib/utils';
+
 export default function Live2D() {
   const modelUrl = 'https://xxx.tgftgf.workers.dev/103302/model.json';
   const [modelData, setModelData] = useState(null);
@@ -22,52 +23,55 @@ export default function Live2D() {
     if (modelData) {
       Promise.all([
         loadExternalResource('https://pixijs.download/v7.3.2/pixi.min.js', 'js')
-      ]).then((e) => {
-        // 创建 Live2D 模型
-        const settings = new PIXI.live2d.Cubism4ModelSettings(modelData);
-        const live2dSprite = PIXI.live2d.Live2DModel.from(settings, {
-          eyeBlink: true,
-          lipSyncWithSound: true,
-          debugLog: false,
-          debugMouseLog: false,
-          randomMotion: false,
-          defaultMotionGroup: 'Motion',
-          autoInteract: follow,
-          expressionFadingDuration: 0,
-          motionFadingDuration: 0,
-          idleMotionFadingDuration: 0
+      ]).then(() => {
+        // 导入 Pixi.js 对象
+        import('pixi.js').then((PIXI) => {
+          // 创建 Live2D 模型
+          const settings = new PIXI.live2d.Cubism4ModelSettings(modelData);
+          const live2dSprite = PIXI.live2d.Live2DModel.from(settings, {
+            eyeBlink: true,
+            lipSyncWithSound: true,
+            debugLog: false,
+            debugMouseLog: false,
+            randomMotion: false,
+            defaultMotionGroup: 'Motion',
+            autoInteract: follow,
+            expressionFadingDuration: 0,
+            motionFadingDuration: 0,
+            idleMotionFadingDuration: 0
+          });
+
+          // 创建 Pixi Application
+          const app = new PIXI.Application({
+            width: 1280,
+            height: 1600,
+            transparent: true,
+            preserveDrawingBuffer: true
+          });
+
+          // 将 Live2D 模型添加到 Pixi Application 的舞台
+          app.stage.addChild(live2dSprite);
+          live2dSprite.scale.set(0.5, 0.5);
+
+          // 点击事件处理
+          function handleModelClick() {
+            // 随机切换表情
+            live2dSprite.internalModel.motionManager.expressionManager.setRandomExpression();
+
+            // 随机切换动作
+            live2dSprite.internalModel.motionManager.startRandomMotion('Motion');
+          }
+
+          // 将点击事件处理函数绑定到 Live2D 模型
+          live2dSprite.on('click', handleModelClick);
+
+          // 将 Pixi Application 挂载到页面上的 DOM 元素
+          const container = document.getElementById('live2d-container');
+          container.style.width = '320px';
+          container.style.height = '400px';
+          container.appendChild(app.view);
         });
-
-        // 创建 Pixi Application
-        const app = new PIXI.Application({
-          width: 1280,
-          height: 1600,
-          transparent: true,
-          preserveDrawingBuffer: true
-        });
-
-        // 将 Live2D 模型添加到 Pixi Application 的舞台
-        app.stage.addChild(live2dSprite);
-        live2dSprite.scale.set(0.5, 0.5);
-
-        // 点击事件处理
-        function handleModelClick() {
-          // 随机切换表情
-          live2dSprite.internalModel.motionManager.expressionManager.setRandomExpression();
-
-          // 随机切换动作
-          live2dSprite.internalModel.motionManager.startRandomMotion('Motion');
-        }
-
-        // 将点击事件处理函数绑定到 Live2D 模型
-        live2dSprite.on('click', handleModelClick);
-
-        // 将 Pixi Application 挂载到页面上的 DOM 元素
-        const container = document.getElementById('live2d-container');
-        container.style.width = '320px';
-        container.style.height = '400px';
-        container.appendChild(app.view);
-      })
+      });
     }
   }, [modelData]);
 
