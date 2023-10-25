2e5e29f3b8e2f13b62820c1a489065fe9afafdf5
realwuliao <25335782+realwuliao@users.noreply.github.com>
2023-10-25 13:34:58 +0800

    0

--- components/Live2D.js
+++ components/Live2D.js
@@ -19,55 +19,56 @@ export default function Live2D() {
   }, [modelUrl]);
 
   useEffect(() => {
-    if (modelData) { Promise.all([
+    if (modelData) {
+      Promise.all([
         loadExternalResource('https://pixijs.download/v7.3.2/pixi.min.js', 'js')
       ]).then((e) => {
-      // 创建 Live2D 模型
-      const settings = new PIXI.live2d.Cubism4ModelSettings(modelData);
-      const live2dSprite = PIXI.live2d.Live2DModel.from(settings, {
-        eyeBlink: true,
-        lipSyncWithSound: true,
-        debugLog: false,
-        debugMouseLog: false,
-        randomMotion: false,
-        defaultMotionGroup: 'Motion',
-        autoInteract: follow,
-        expressionFadingDuration: 0,
-        motionFadingDuration: 0,
-        idleMotionFadingDuration: 0
-      });
+        // 创建 Live2D 模型
+        const settings = new PIXI.live2d.Cubism4ModelSettings(modelData);
+        const live2dSprite = PIXI.live2d.Live2DModel.from(settings, {
+          eyeBlink: true,
+          lipSyncWithSound: true,
+          debugLog: false,
+          debugMouseLog: false,
+          randomMotion: false,
+          defaultMotionGroup: 'Motion',
+          autoInteract: follow,
+          expressionFadingDuration: 0,
+          motionFadingDuration: 0,
+          idleMotionFadingDuration: 0
+        });
 
-      // 创建 Pixi Application
-      const app = new PIXI.Application({
-        width: 1280,
-        height: 1600,
-        transparent: true,
-        preserveDrawingBuffer: true
-      });
+        // 创建 Pixi Application
+        const app = new PIXI.Application({
+          width: 1280,
+          height: 1600,
+          transparent: true,
+          preserveDrawingBuffer: true
+        });
 
-      // 将 Live2D 模型添加到 Pixi Application 的舞台
-      app.stage.addChild(live2dSprite);
-      live2dSprite.scale.set(0.5, 0.5);
+        // 将 Live2D 模型添加到 Pixi Application 的舞台
+        app.stage.addChild(live2dSprite);
+        live2dSprite.scale.set(0.5, 0.5);
 
-      // 点击事件处理
-      function handleModelClick() {
-        // 随机切换表情
-        live2dSprite.internalModel.motionManager.expressionManager.setRandomExpression();
+        // 点击事件处理
+        function handleModelClick() {
+          // 随机切换表情
+          live2dSprite.internalModel.motionManager.expressionManager.setRandomExpression();
 
-        // 随机切换动作
-        live2dSprite.internalModel.motionManager.startRandomMotion('Motion');
-      }
+          // 随机切换动作
+          live2dSprite.internalModel.motionManager.startRandomMotion('Motion');
+        }
 
-      // 将点击事件处理函数绑定到 Live2D 模型
-      live2dSprite.on('click', handleModelClick);
+        // 将点击事件处理函数绑定到 Live2D 模型
+        live2dSprite.on('click', handleModelClick);
 
-      // 将 Pixi Application 挂载到页面上的 DOM 元素
-      const container = document.getElementById('live2d-container');
-      container.style.width = '320px';
-      container.style.height = '400px';
-      container.appendChild(app.view);
-    })
-  }
+        // 将 Pixi Application 挂载到页面上的 DOM 元素
+        const container = document.getElementById('live2d-container');
+        container.style.width = '320px';
+        container.style.height = '400px';
+        container.appendChild(app.view);
+      })
+    }
   }, [modelData]);
 
   return <div id='live2d-container' style={{ width: '320px', height: '400px' }}></div>;
